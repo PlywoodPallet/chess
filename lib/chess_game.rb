21bce364642847ec623e620445750e6fb78e34f3
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2022-09-08 17:40:13 -0700

    Create ChessGame class and add notes

---
+++ lib/chess_game.rb
@@ -0,0 +1,77 @@
+# User selects chess piece
+# Game provides list of legal moves (different based on piece type)
+# User choses a legal move
+# Game moves piece and removes opposing player's piece if needed
+
+# Legal moves
+# Player can only move one's pieces
+# Moves that intersect with own pieces are not listed (exception: castling)
+# Moves that capture an enemy piece are listed (some extra boundaries based on chess piece, like a rook can't jump over a piece while knight can)
+
+# Legal moves by piece
+# King - one square in any direction (+ castling)
+# Rook - move along rank or file, cannot jump, (+ castling)
+# Bishop - move diagonally, cannot jump
+# Queen - Rook + Bishop
+# Knight - (+ or -) 1/2 or 2/1, CAN jump
+# Pawn - Moves one forward or two forward if first move. Can only capture pieces one diagonally. En passant and promotion
+
+# Game over conditions
+# Check - King is under threat, player must only move the king next turn
+# Checkmate - King is under threat and has no moves
+# Player resign
+
+# Esoteric moves (implement later)
+# Castling between King and Rook
+# - not a legal response to a check
+# En Passant between two pawns
+# promotion of a pawn that reaches the eigth rank (row)
+
+# Game over - Draw (implement later)
+# Stalemate - player has no legal move and not in check
+# Dead position - neither player is able to checkmate. Such as only two kings are on the board (research all possibilities - hope its a short list)
+# Draw by agreement - can only be offered after move 30. Implementation not needed
+# Threefold repetition - situation too esoteric
+# Fifty-move-rule - situation too esoteric
+
+# pieces are denoted by the file its in such as "pawn in f-file"
+
+class ChessGame
+  def initialize
+    @active_player = 1
+    @player1_input = nil
+    @player2_input = nil
+    @board = ChessBoard.new
+  end
+
+  def play_game
+    puts "Greeting message"
+    print_board
+    turn_order until game_over?
+    puts "Final message"
+  end
+
+  def turn_order
+    player_turn(@active_player)
+    print_board
+    toggle_active_player
+  end
+
+  # Switch the active player between 1 and 2
+  def toggle_active_player
+    if @active_player == 1
+      @active_player = 2
+    else
+      @active_player = 1
+    end
+  end
+
+  def game_over?
+    true
+  end
+
+  # call a ChessBoard method here
+  def print_board
+    puts "Print the chess board"
+  end
+end
