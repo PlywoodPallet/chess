6862d552e021e4851528169b197e2d333bb0d9a2
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2022-09-08 23:27:11 -0700

    Add populate all subclasses with unicode icon of piece. Implement #to_s

--- lib/chess_piece.rb
+++ lib/chess_piece.rb
@@ -1,37 +1,81 @@
-# player = 1 or 2
-# unicode icon (white or black)
-
 # Unneccessary
 # location? - its stored in Board, pieces are chosen by grid coordinate
 # legal moves? calculated by Game
 
-# Pawn
-# Knight
-# Rook
-# Bishop
-# Queen
-# King
+# Player 1 = white
+# Player 2 = black
+# according to wikipedia
+
+# Notes
+# The "white" pieces look like black
+# the "black" pieces look like white
 
 class ChessPiece
-  def initialize (player = nil)
+  attr_reader :player, :icon
+
+  # player is nil by default
+  def initialize(player = nil)
     @player = player
+    @icon = nil
+  end
+
+  # important for Chess_Board.print_board
+  def to_s
+    @icon
   end
 end
 
 class Pawn < ChessPiece
+  def initialize(player = nil)
+    super
+    
+    # white : black
+    @icon = @player == 1 ? "\u2659" : "\u265F"
+  end
 end
 
 class Knight < ChessPiece
+  def initialize(player = nil)
+    super
+    
+    # white : black
+    @icon = @player == 1 ? "\u2658" : "\u265E"
+  end
 end
 
 class Rook < ChessPiece
+  def initialize(player = nil)
+    super
+    
+    # white : black
+    @icon = @player == 1 ? "\u2656" : "\u265C"
+  end
 end
 
 class Bishop < ChessPiece
+  def initialize(player = nil)
+    super
+    
+    # white : black
+    @icon = @player == 1 ? "\u2657" : "\u265D"
+  end
 end
 
 class Queen < ChessPiece
+  def initialize(player = nil)
+    super
+    
+    # white : black
+    @icon = @player == 1 ? "\u2655" : "\u265B"
+  end
 end
 
 class King < ChessPiece
+  def initialize(player = nil)
+    super
+    @hasCastled = false
+    
+    # white : black
+    @icon = @player == 1 ? "\u2654" : "\u265A"
+  end
 end
