f0b104162cd3d7c20510d6f0b517bf6128f26c1b
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2022-09-09 00:32:20 -0700

    Rename to #initialize_player_pieces and implement

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -5,13 +5,7 @@
 # rows are called ranks
 # columns are called files
 
-# Per Team - 16 pieces
-# Pawn - 8
-# Knight - 2
-# Rook - 2
-# Bishop - 2
-# Queen - 1
-# King - 1
+
 
 # Board alignment - "queen starts on own color". White queen starts on white square, etc
 
@@ -20,6 +14,9 @@
 
 # optional: alternating square color
 # https://en.wikipedia.org/wiki/Chess if following this format, white is bottom two rows, black is top two rows
+
+
+require '../lib/chess_piece.rb'
 
 class ChessBoard
   def initialize
@@ -32,7 +29,9 @@ class ChessBoard
     # Knights Travails written for a 2d array - can use a lookup 2d array that converts between coordinate key "a3" and y,x coordinates
     # Major/minor diagonal algo in Connect Four project written for 2d array
     @board = Hash.new # default value is nil so moves off the board can be detected
-    initialize_board
+    initialize_board_coordinates
+    initialize_player_pieces(1)
+    initialize_player_pieces(2)
   end
 
   # initialize the board coordinates according to the format below (rows: descending numbers, col: ascending alphabet)
@@ -45,8 +44,61 @@ class ChessBoard
     end
   end
 
-  def initialize_board_pieces
-    
+  # initialization is hard coded beacuse the board is the same at the start of every chess game
+  # TODO: Can I practice using a lamda or proc here?
+  # White = player 1, Black = player 2
+  def initialize_player_pieces(player_num)
+    back_row = nil
+    pawn_row = nil
+
+    if player_num == 1
+      back_row = 1
+      pawn_row = 2
+    elsif player_num == 2
+      back_row = 8
+      pawn_row = 7
+    else
+      puts "Invalid player number"
+      # Learn how to throw errors
+    end
+
+    # White: 8x Pawn - a2 to h2
+    # Black: 8x Pawn - a7 to h7
+    ('a'..'h').each do |letter|
+      a_pawn = Pawn.new(player_num)
+      @board["#{letter}#{pawn_row}"] = a_pawn
+    end
+
+    # White: 2x Rook - a1, h1
+    # Black: 2x Rook - a8, h8
+    ['a', 'h'].each do |letter|
+      a_rook = Rook.new(player_num)
+      @board["#{letter}#{back_row}"] = a_rook
+    end
+
+    # White: 2x Knight - b1, g1
+    # Black: 2x Knight - b8, g8
+    ['b', 'g'].each do |letter|
+      a_knight = Knight.new(player_num)
+      @board["#{letter}#{back_row}"] = a_knight
+    end
+
+    # White: 2x Bishop - c1, f1
+    # Black: 2x Bishop - c8, f8
+    ['c', 'f'].each do |letter|
+      a_bishop = Bishop.new(player_num)
+      @board["#{letter}#{back_row}"] = a_bishop
+    end
+
+    # White: 1x Queen - d1
+    # Black: 1x Queen - d8
+    queen = Queen.new(player_num)
+    @board["d#{back_row}"] = queen
+
+    # White: 1x King - e1
+    # Black: 1x King - e8
+    king = King.new(player_num)
+    @board["e#{back_row}"] = king
   end
 
   # print the board according to the format below (rows: descending numbers, col: ascending alphabet)
@@ -55,7 +107,7 @@ class ChessBoard
     (1..8).reverse_each do |number|
       ('a'..'h').each do |letter|
         print @board["#{letter}#{number}"]
-        print " " # space in between items
+        print '  ' # space in between items
       end
       puts # new line
     end
@@ -69,7 +121,7 @@ class ChessBoard
           print "#{letter}#{number}:" 
           print @board["#{letter}#{number}"]
           
-          print " " # space in between items
+          print '  ' # space in between items
         end
         puts # new line
       end
