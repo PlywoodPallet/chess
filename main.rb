1ed42eb562e98f2b33afed525587a593abd11e93
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2022-09-19 16:23:54 -0700

    Add tests for #get_valid_knight_moves

--- main.rb
+++ main.rb
@@ -4,8 +4,8 @@ require_relative 'lib/chess_piece'
 require_relative 'lib/string' # console font styles
 
 board = ChessBoard.new
-
-board.move_piece("b1", "c6")
+board.move_piece("b8", "c3")
 board.print_board
-piece = board.get_piece("c6")
-p board.get_valid_knight_moves(piece, "c6")
+piece = board.get_piece("c3")
+
+p board.get_valid_knight_moves(piece, "c3")
