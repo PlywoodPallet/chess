8557b075700bfea6305550230abf2b9b87dbc63c
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2022-09-23 17:34:10 -0700

    Fix object recognition bug in #get_valid_moves

--- main.rb
+++ main.rb
@@ -4,9 +4,9 @@ require_relative 'lib/chess_piece'
 require_relative 'lib/string' # console font styles
 
 board = ChessBoard.new
-board.move_piece("e8", "e3")
+board.move_piece('b1', 'd4')
 board.print_board
-p board.get_valid_king_moves("e3")
+p board.get_valid_moves('d4')
 
 # queen = Queen.new(1)
 # p queen.relative_moves
