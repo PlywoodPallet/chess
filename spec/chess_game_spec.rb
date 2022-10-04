b5c7991cf86ce77ce76a1c48f9a345685dd4cca7
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2022-10-04 15:48:09 -0700

    create test for #check?

--- spec/chess_game_spec.rb
+++ spec/chess_game_spec.rb
@@ -1,1 +1,33 @@
 #test all conditions of #verify_start_coord
+
+require_relative '../lib/chess_board'
+require_relative '../lib/chess_piece'
+require_relative '../lib/chess_game'
+
+describe ChessGame do
+
+  describe '#check?' do
+    subject(:game) { described_class.new }
+    it 'returns true when king is threatened' do 
+      board = game.board
+
+      board.move_piece('e1', 'd4') # move king into position
+      board.move_piece('d2', 'd3') # block king with own pawn
+      board.move_piece('a8', 'c4') # move opponent rook to threaten king
+      board.move_piece('b8', 'b5') # move opponent knight to threaten king
+
+      check = game.check?(1)
+
+      expect(check).to eq(true)
+    end
+
+    it 'returns false when king is safe' do 
+      # use the default state of the board
+      check = game.check?(1)
+
+      expect(check).to eq(false)
+    end
+  end
+
+
+end
