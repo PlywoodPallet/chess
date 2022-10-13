74baeb54b8fde716e8ac8d1daf4797a6772c101d
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2022-10-13 14:46:23 -0700

    test commit

--- main.rb
+++ main.rb
@@ -5,13 +5,10 @@ require_relative 'lib/string' # console font styles
 
 
 game = ChessGame.new
-board = game.board
-board.move_piece('e1', 'd4') # move king into position
-board.move_piece('d2', 'd3') # block king with own pawn
-board.move_piece('a8', 'c4') # move opponent rook to threaten king
-board.move_piece('b8', 'b5') # move opponent knight to threaten king
+board = game.chess_board
 
-game.play_game
+board.move_piece('e2', 'f3') # move pawn out of the way
+board.move_piece('e7', 'f6') # move pawn out of the way
 
 
-# DID I CREATE A TEST FOR THIS? I think the problem here is that get_threatening_pieces assumes the player_num/opponent_player_num. player_num needs to be a parameter so I can find the threatening pieces for a consistent player 
+board.print_board
--- spec/chess_game_spec.rb
+++ spec/chess_game_spec.rb
@@ -11,7 +11,7 @@ describe ChessGame do
   describe '#check?' do
     subject(:game) { described_class.new }
     it 'returns true when king is threatened' do 
-      board = game.board
+      board = game.chess_board
 
       board.move_piece('e1', 'd4') # move king into position
       board.move_piece('d2', 'd3') # block king with own pawn
@@ -23,77 +23,77 @@ describe ChessGame do
       expect(check).to eq(true)
     end
 
-    it 'returns false when king is safe' do 
-      # use the default state of the board
-      check = game.check?(1)
+  #   it 'returns false when king is safe' do 
+  #     # use the default state of the board
+  #     check = game.check?(1)
 
-      expect(check).to eq(false)
-    end
+  #     expect(check).to eq(false)
+  #   end
   end
 
-  describe '#checkmate?' do
-    subject(:game) { described_class.new }
-    it 'returns true when king is under threat and has no legal moves' do 
-      board = game.board
+  # describe '#checkmate?' do
+  #   subject(:game) { described_class.new }
+  #   it 'returns true when king is under threat and has no legal moves' do 
+  #     board = game.chess_board
 
-      board.move_piece('e1', 'd4')
-      board.move_piece('d2', 'd3')
-      board.move_piece('a8', 'c4')
-      board.move_piece('b8', 'b5')
-      board.move_piece('d7', 'd5')
-      board.move_piece('d8', 'e4')
+  #     board.move_piece('e1', 'd4')
+  #     board.move_piece('d2', 'd3')
+  #     board.move_piece('a8', 'c4')
+  #     board.move_piece('b8', 'b5')
+  #     board.move_piece('d7', 'd5')
+  #     board.move_piece('d8', 'e4')
 
-      checkmate = game.checkmate?(1)
+  #     checkmate = game.checkmate?(1)
 
-      expect(checkmate).to eq(true)
+  #     expect(checkmate).to eq(true)
 
-    end
+  #   end
 
-    it 'returns false when king is under check' do 
-      # use the default state of the board
-      board = game.board
+  #   it 'returns false when king is under check' do 
+  #     # use the default state of the board
+  #     board = game.chess_board
 
-      board.move_piece('e1', 'd4') # move king into position
-      board.move_piece('d2', 'd3') # block king with own pawn
-      board.move_piece('a8', 'c4') # move opponent rook to threaten king
-      board.move_piece('b8', 'b5') # move opponent knight to threaten king
+  #     board.move_piece('e1', 'd4') # move king into position
+  #     board.move_piece('d2', 'd3') # block king with own pawn
+  #     board.move_piece('a8', 'c4') # move opponent rook to threaten king
+  #     board.move_piece('b8', 'b5') # move opponent knight to threaten king
 
-      checkmate = game.checkmate?(1)
+  #     checkmate = game.checkmate?(1)
 
-      expect(checkmate).to eq(false)
-    end
+  #     expect(checkmate).to eq(false)
+  #   end
 
-    it 'returns false when king is not threatened' do 
-      # use the default state of the board
-      check = game.checkmate?(1)
+  #   it 'returns false when king is not threatened' do 
+  #     # use the default state of the board
+  #     check = game.checkmate?(1)
 
-      expect(check).to eq(false)
-    end
-  end
+  #     expect(check).to eq(false)
+  #   end
+  # end
 
-  # TODO: Use rspec to play the game rather than setting up the board manually
-  describe '#game_over? / #print_final_message' do
-    subject(:game) { described_class.new }
-    xit 'When P1 is checkmated, declare the victory condition and P2 as the winner' do
-      board = game.board
+  # # TODO: Use rspec to play the game rather than setting up the board manually
+  # describe '#game_over? / #print_final_message' do
+  #   subject(:game) { described_class.new }
+  #   xit 'When P1 is checkmated, declare the victory condition and P2 as the winner' do
+  #     board = game.chess_board
       
-      # TODO: Use rspec to play the game rather than setting up the board manually
+  #     # TODO: Use rspec to play the game rather than setting up the board manually
 
-      # Fool's Mate (quick checkmate)
-      board.move_piece('f2', 'f3') # p1
-      board.move_piece('e7', 'e5') # p2
-      board.move_piece('g2', 'g4') # p1
-      board.move_piece('d8', 'h4') # p2
+  #     # Fool's Mate (quick checkmate)
+  #     board.move_piece('f2', 'f3') # p1
+  #     board.move_piece('e7', 'e5') # p2
+  #     board.move_piece('g2', 'g4') # p1
+  #     board.move_piece('d8', 'h4') # p2
       
-      # p1 is @active_player by default
-      game.play_game
+  #     # p1 is @active_player by default
+  #     game.play_game
 
-      correct_message = "Checkmate! Player 2 wins"
+  #     correct_message = "Checkmate! Player 2 wins"
 
-      # TODO: how to test for output? rspec says .to output() is depreciated
-      expect(game.print_final_message).to eq(correct_message)
-    end
-  end
+  #     # TODO: how to test for output? rspec says .to output() is depreciated
+  #     expect(game.print_final_message).to eq(correct_message)
+  #   end
+  # end
 
   
 end
