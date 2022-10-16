cd53a69f10b4b65d0a4e79a2bc3c2092bf7ce9c6
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2022-10-16 10:56:07 -0700

    File creation

---
+++ lib/game_recorder.rb
@@ -0,0 +1,11 @@
+# unknown if all these dependencies are necessary for final version
+require_relative '../lib/chess_board'
+require_relative '../lib/move_validator'
+require_relative '../lib/chess_piece'
+
+class GameRecorder
+
+  def initialize
+  end
+
+end
