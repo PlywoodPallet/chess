b34589fb1078d7d496b09d3bbd82a38d6e961531
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2022-10-19 13:35:41 -0700

    Move comments to top

--- main.rb
+++ main.rb
@@ -1,9 +1,3 @@
-require_relative 'lib/chess_game'
-require_relative 'lib/chess_board'
-require_relative 'lib/chess_piece'
-require_relative 'lib/move_validator'
-require_relative 'lib/string' # console font styles
-
 # Accomplishments
 # Broke a lot of tests when designing remove_moves_jeopardize_king. Fixed by doing a major refactor of valid_moves. Split into valid_moves and estimate_moves
 # LOTS of edge cases!
@@ -26,18 +20,19 @@ require_relative 'lib/string' # console font styles
 # Import a ruby chess agent to be the automatic player 2
 # Create a GameMessages module to take a lot of code out of ChessGame to make it more readable
 
-game = ChessGame.new
-board = game.chess_board
+require_relative 'lib/chess_game'
+require_relative 'lib/chess_board'
+require_relative 'lib/chess_piece'
+require_relative 'lib/move_validator'
+require_relative 'lib/string' # console font styles
 
-exceptions_array = ['e8', 'e1', 'd1']
-board.clear_board(exceptions_array)
+p Dir.exist? 'saved_games'
+p Dir.entries('saved_games/.')
+p Dir.empty?('saved_games')
+p Dir.empty?('empty_directory')
 
 
-board.print_board
+# game = ChessGame.new
+# game.play_game
 
-p board.any_available_moves?
-
-# testing any_available_moves? and statlemate
-# need a ChessBoard clear_board(exceptions_array = nil) method. Clears all pieces except the ones in array
-# place pieces into stalemate position
-# test if statelemate? = true
+load_game.play
